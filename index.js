768860ad7f77959ec3d991cbf96d6465139758b2
aAnish07 <ch20btech11006@iith.ac.in>
2021-08-13 01:45:08 +0530

    dynamic ports

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log("Server up and running");
 })
 
